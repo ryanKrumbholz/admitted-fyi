d1597f4bc95d94d4948f7a280bce9998f1297768
Ryan Krumbholz <krumbholz98@gmail.com>
2024-02-05 21:01:05 -0800

    updates

--- src/env.js
+++ src/env.js
@@ -4,10 +4,11 @@ import { z } from 'zod'
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
+   * isn'nt built with invalid env vars.
    */
   server: {
-    AUTH_PROVIDER: z.enum(['github', 'okta']),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
     DATABASE_URL: z
       .string()
       .url()
@@ -15,12 +16,6 @@ export const env = createEnv({
         (str) => !str.includes('YOUR_MYSQL_URL_HERE'),
         'You forgot to change the default URL',
       ),
-    GITHUB_ID:
-      process.env.AUTH_PROVIDER === 'github' ? z.string() : z.undefined(),
-    GITHUB_SECRET:
-      process.env.AUTH_PROVIDER === 'github' ? z.string() : z.undefined(),
-    GITHUB_ALLOWED_ORG:
-      process.env.AUTH_PROVIDER === 'github' ? z.string() : z.undefined(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
       .default('development'),
@@ -36,19 +31,6 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    OKTA_CLIENT_ID:
-      process.env.AUTH_PROVIDER === 'okta' ? z.string() : z.undefined(),
-    OKTA_CLIENT_SECRET:
-      process.env.AUTH_PROVIDER === 'okta' ? z.string() : z.undefined(),
-    OKTA_ISSUER:
-      process.env.AUTH_PROVIDER === 'okta' ? z.string() : z.undefined(),
-    CLOUDINARY_CLOUD_NAME: z.string(),
-    CLOUDINARY_API_KEY: z.string(),
-    CLOUDINARY_API_SECRET: z.string(),
-    ENABLE_SLACK_POSTING: z.coerce.boolean().default(false).optional(),
-    SLACK_WEBHOOK_URL: Boolean(process.env.ENABLE_SLACK_POSTING === 'true')
-      ? z.string()
-      : z.string().optional(),
   },
 
   /**
@@ -56,37 +38,20 @@ export const env = createEnv({
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
-    NEXT_PUBLIC_ENABLE_IMAGE_UPLOAD: z.coerce
-      .boolean()
-      .default(false)
-      .optional(),
-  },
+  client: {},
 
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    AUTH_PROVIDER: process.env.AUTH_PROVIDER,
     DATABASE_URL: process.env.DATABASE_URL,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    GITHUB_ALLOWED_ORG: process.env.GITHUB_ALLOWED_ORG,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_APP_URL: process.env.NEXT_APP_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_ENABLE_IMAGE_UPLOAD:
-      process.env.NEXT_PUBLIC_ENABLE_IMAGE_UPLOAD,
-    OKTA_CLIENT_ID: process.env.OKTA_CLIENT_ID,
-    OKTA_CLIENT_SECRET: process.env.OKTA_CLIENT_SECRET,
-    OKTA_ISSUER: process.env.OKTA_ISSUER,
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-    ENABLE_SLACK_POSTING: process.env.ENABLE_SLACK_POSTING,
-    SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,9 +5,8 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import OktaProvider from 'next-auth/providers/okta'
-
+import GoogleProvider from 'next-auth/providers/google'
+import AppleProvider from 'next-auth/providers/apple'
 import { env } from '~/env.js'
 import { db } from '~/server/db'
 
@@ -45,48 +44,21 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   adapter: PrismaAdapter(db),
-  providers:
-    env.AUTH_PROVIDER === 'github'
-      ? [
-          GithubProvider({
-            clientId: env.GITHUB_ID!,
-            clientSecret: env.GITHUB_SECRET!,
-            authorization:
-              'https://github.com/login/oauth/authorize?scope=read:user+user:email+read:org',
-            userinfo: {
-              url: 'https://api.github.com/user',
-              async request({ client, tokens }) {
-                const profile = await client.userinfo(tokens.access_token!)
-
-                const res = await fetch('https://api.github.com/user/orgs', {
-                  headers: {
-                    Authorization: `token ${tokens.access_token}`,
-                  },
-                })
-
-                const userOrgs = (await res.json()) as { login: string }[]
-
-                // Set flag to deny signIn if allowed org is not found in the user organizations
-                if (
-                  !userOrgs.find((org) => org.login === env.GITHUB_ALLOWED_ORG)
-                ) {
-                  profile.notAllowed = true
-                }
-
-                return profile
-              },
-            },
-          }),
-        ]
-      : env.AUTH_PROVIDER === 'okta'
-        ? [
-            OktaProvider({
-              clientId: env.OKTA_CLIENT_ID!,
-              clientSecret: env.OKTA_CLIENT_SECRET!,
-              issuer: env.OKTA_ISSUER,
-            }),
-          ]
-        : [],
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    //    AppleProvider({
+    //      clientId: env.APPLE_ID,
+    //      clientSecret: env.APPLE_SECRET,
+    //    }),
+    //    EmailProvider({
+    //      server: env.EMAIL_SERVER,
+    //      from: env.EMAIL_FROM,
+    //      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    //    }),
+  ],
   pages: {
     signIn: '/sign-in',
     signOut: '/sign-in',
