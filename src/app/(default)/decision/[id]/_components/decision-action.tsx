42ca04f917ed4a595f1f541d253e7fb6741655f3
Ryan Krumbholz <krumbholz98@gmail.com>
2024-02-25 17:21:06 -0800

    rm files

--- src/app/(default)/decision/[id]/_components/decision-action.tsx
+++ src/app/(default)/decision/[id]/_components/decision-action.tsx
@@ -3,29 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { toast } from 'react-hot-toast'
 import { Button } from '~/app/_components/button'
-import {
-  AlertDialogAction,
-  AlertDialogActions,
-  AlertDialogCloseButton,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogCancel,
-} from '~/app/_components/alert-dialog'
-import {
-  Menu,
-  MenuButton,
-  MenuItemButton,
-  MenuItems,
-  MenuItem,
-  MenuItemsContent,
-  MenuItemLink,
-} from '~/app/_components/menu'
 import { useDialogStore } from '~/app/_hooks/use-dialog-store'
-import DotsIcon from '~/app/_svg/dots-icon'
-import EditIcon from '~/app/_svg/edit-icon'
-import EyeIcon from '~/app/_svg/eye-icon'
-import TrashIcon from '~/app/_svg/trash-icon'
 import { api } from '~/trpc/react'
 
 type DecisionActionProps = {
